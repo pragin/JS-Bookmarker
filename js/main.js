66671cf4714be6ed7e65d8351f4b7c8ce2f8c7f6
praginraj <pragin.martin@hotmail.com>
2018-05-28 19:45:27 +0100

    Add fetchBookmark()

--- js/main.js
+++ js/main.js
@@ -42,3 +42,31 @@ function saveBookmark(e){
     console.log("not cancelable");
   }
 }
+
+//Fetch bookmarks from the localStorage
+
+/*
+  *This function retrieve the bookmarks from localStorage and display it in a div called bookmarkResults.
+  *This function also has visit and delete buttons for each bookmark.
+*/
+function fetchBookmarks(){
+  //Get existing bookmars from localStorage
+  var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+  //Select the div
+  var bookmarkResults = document.getElementById('bookmarkResults');
+
+  //Build output
+  bookmarkResults.innerHTML = "";
+
+  for(var i = 0; i < bookmarks.length; i++){
+    var name = bookmarks[i].name;
+    var url = bookmarks[i].url;
+
+    bookmarkResults.innerHTML += '<div class="well">'
+                                + '<h3>' + name
+                                + ' <a class = "btn btn-default" target="_blank" href="' + url +'">Visit</a> '
+                                + ' <a onclick="deleteBookmark(\'' + url + '\')" class = "btn btn-danger" target="_blank" href=#>Delete</a> '
+                                + '</h3></div>';
+  //  console.log(name);
+  }
+}
