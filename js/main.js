16d2a72f53f1eaf0406a15d54ac403a1c9736d42
praginraj <pragin.martin@hotmail.com>
2018-05-28 18:57:50 +0100

    Add saveBookmark function to main.js

---
+++ js/main.js
@@ -0,0 +1,44 @@
+//Listen for form submit
+document.getElementById('myForm').addEventListener('submit', saveBookmark);
+/*
+  *This function captures the form input values on submit
+  *It then creates an array containing site name and URL
+  *If bookmarks already exists then add the new bookmark to the array and store it
+  *else add a new bookmarks to localStorage
+
+*/
+function saveBookmark(e){
+//Capture the form inputs
+  var siteName = document.getElementById('siteName').value;
+  var siteUrl = document.getElementById('siteUrl').value;
+
+  //objact to be passed to the local storage
+  var bookmark = {
+    name : siteName,
+    url : siteUrl
+  }
+
+  //save the bookmare object to local storage
+  if(localStorage.getItem('bookmarks') === null){
+    //Init array
+    var bookmarks = [];
+    //add bookmark to array
+    bookmarks.push(bookmark);
+    //Set to  localStorage
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+
+  }else{
+    //Get existing bookmars from localStorage
+    var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+    //add bookmark to array
+    bookmarks.push(bookmark);
+    //Re-set to localStorage
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+  }
+  //Prevent from form submitting if it is cancelable
+  if(e.cancelable){
+    e.preventDefault();
+  }else {
+    console.log("not cancelable");
+  }
+}
